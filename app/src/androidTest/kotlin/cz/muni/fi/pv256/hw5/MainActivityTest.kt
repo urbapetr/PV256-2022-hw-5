ea112b81cc3e95bec07b2e62692ca8eef372b230
Petr Urbánek <514122@mail.muni.cz>
2022-04-01 12:19:05 +0200

    homework finished

--- app/src/androidTest/kotlin/cz/muni/fi/pv256/hw5/MainActivityTest.kt
+++ app/src/androidTest/kotlin/cz/muni/fi/pv256/hw5/MainActivityTest.kt
@@ -3,19 +3,20 @@ package cz.muni.fi.pv256.hw5
 import android.view.View
 import android.widget.TextView
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.hamcrest.Matcher
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.lang.Integer.parseInt
+
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
