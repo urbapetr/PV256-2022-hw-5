ebb4a2384dfb593d01732c21b2e67cd9011d9001
Petr Urbánek <514122@mail.muni.cz>
2022-04-01 10:49:40 +0200

    homework finished

--- app/src/androidTest/kotlin/cz/muni/fi/pv256/hw5/MainActivityTest.kt
+++ app/src/androidTest/kotlin/cz/muni/fi/pv256/hw5/MainActivityTest.kt
@@ -3,14 +3,14 @@ package cz.muni.fi.pv256.hw5
 import android.view.View
 import android.widget.TextView
 import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.hamcrest.Matcher
 import org.junit.Test
@@ -49,18 +49,18 @@ class MainActivityTest {
         val stringHolder = arrayOf<String?>(null)
         onView(matcher).perform(
             object : ViewAction {
-            override fun getConstraints(): Matcher<View> {
-                return isAssignableFrom(TextView::class.java)
-            }
-            override fun getDescription(): String {
-                return "getting text from a TextView"
-            }
+                override fun getConstraints(): Matcher<View> {
+                    return isAssignableFrom(TextView::class.java)
+                }
+                override fun getDescription(): String {
+                    return "getting text from a TextView"
+                }
 
-            override fun perform(uiController: UiController?, view: View) {
-                val tv = view as TextView // Save, because of check in getConstraints()
-                stringHolder[0] = tv.text.toString()
+                override fun perform(uiController: UiController?, view: View) {
+                    val tv = view as TextView // Save, because of check in getConstraints()
+                    stringHolder[0] = tv.text.toString()
+                }
             }
-        }
         )
         return stringHolder[0]
     }
